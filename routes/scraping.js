a5945035cc3771b4487327f6098d1df4898bdb8c
strutive07 <strutive07@gmail.com>
2019-05-27 00:55:15 +0900

    빌드

--- routes/scraping.js
+++ routes/scraping.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const json_parser = require('../core/json_parser');
-const exec = require('child_process').exec;
 
 router.get('/health_check', (req, res, next) => {
-    exec('npm install chromedriver --chromedriver-force-download', function(err, stdout, stderr){
-        if (err) {
-            res.status(500).json({status: 500, data: error})
-        }else{
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-            res.status(200).json({
-                status: 200,
-                data: ['ok']
-            });
-        }
+    res.status(200).json({
+        status: 200,
+        data: ['ok']
     });
 });
 
